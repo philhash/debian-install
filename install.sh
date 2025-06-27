1762297f0f3871a038306b6ccececf9c4f726819
Phil Hash <ht.phi99@gmail.com>
2025-06-27 21:46:37 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #Install XFCE
 sudo apt install -y xfce4 xfce4-power-manager xfce4-clipman-plugin xfce4-screenshooter xfce4-taskmanager xfce4-terminal blueman slick-greeter \
-	mousepad ristretto atril vlc ibus-unikey timeshift curl gh firewalld firefox adb fastboot gnome-disk-utility thunar-archive-plugin unrar \
+	mousepad ristretto atril ibus-unikey timeshift curl gh firewalld firefox adb fastboot gnome-disk-utility thunar-archive-plugin unrar \
 	flatpak intel-media-va-driver-non-free i965-va-driver-shaders libavcodec-extra gstreamer1.0-vaapi greybird-gtk-theme \
 	network-manager-gnome network-manager-openvpn-gnome xserver-xorg-video-intel- desktop-base- tango-icon-theme- thunar-volman-
 #Add flathub repo
