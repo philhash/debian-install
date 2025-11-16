91b5fe6f73e8a1467487cf1f1ec530d493b43a71
Phil Hash <ht.phi99@gmail.com>
2025-11-16 01:10:48 -0500

    Remove Brave browser installation from script
    Removed installation steps for Brave browser.

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #Install minimal gnome
 sudo apt install -y gnome-session gnome-console network-manager-gnome network-manager-openvpn-gnome gnome-disk-utility nautilus gnome-shell-pomodoro firefox-esr vlc ibus-unikey timeshift curl gh firewalld adb fastboot unrar flatpak intel-media-va-driver-non-free libavcodec-extra gstreamer1.0-vaapi
-#Install Brave
-sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
-sudo apt update
-sudo apt install -y brave-browser
 #Add flathub repo
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 #hidpi gnome login screen
