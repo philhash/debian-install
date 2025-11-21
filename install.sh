b6603b0ecc81c3fefcd904a2d0f9cc58619ae18e
Phil Hash <ht.phi99@gmail.com>
2025-11-21 10:57:36 +0700

    Install Brave browser and update gnome packages
    Added installation steps for Brave browser and updated package installation command.

--- install.sh
+++ install.sh
@@ -1,7 +1,12 @@
 #Install minimal gnome
-sudo apt install -y gnome-session gnome-console network-manager-gnome network-manager-openvpn-gnome gnome-disk-utility nautilus gnome-shell-pomodoro firefox-esr vlc ibus-unikey timeshift curl gh firewalld adb fastboot unrar flatpak intel-media-va-driver-non-free libavcodec-extra gstreamer1.0-vaapi
+sudo apt install -y gnome-session gnome-console network-manager-gnome network-manager-openvpn-gnome gnome-disk-utility nautilus gnome-shell-pomodoro vlc ibus-unikey timeshift curl gh firewalld adb fastboot unrar flatpak intel-media-va-driver-non-free libavcodec-extra gstreamer1.0-vaapi
 #Add flathub repo
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+#Install Brave
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+sudo apt update
+sudo apt install brave-browser
 #hidpi gnome login screen
 sudo tee /usr/share/glib-2.0/schemas/99_hidpi.gschema.override << EOF
 [org.gnome.desktop.interface]
